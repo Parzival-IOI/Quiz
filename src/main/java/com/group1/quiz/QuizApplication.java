f7e02f6a583d7ad2d7bbb65e04fb3386972992af
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-06-05 19:34:55 +0700

    add key to env

--- src/main/java/com/group1/quiz/QuizApplication.java
+++ src/main/java/com/group1/quiz/QuizApplication.java
@@ -4,8 +4,10 @@ import com.group1.quiz.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 @EnableConfigurationProperties(RsaKeyProperties.class)
 public class QuizApplication {
 
